7db5c8ed6885b0413f7969723765a06f4a44f50a
autumnyard <autumnyardworks@gmail.com>
2017-04-23 15:58:00 +0200

    Añadidos tweens.

---
+++ Assets/_Base/Scripts/Tools/TweenMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class TweenMove : MonoBehaviour {
+
+    public bool playOnStart = true;
+    //public float positionInit = -1f;
+    //public float positionFinish = 2f;
+    public Vector2 positionShift;
+    public float time = 1f;
+    public Ease ease;
+    public bool loop = false;
+    public LoopType loopType;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (playOnStart)
+        {
+            Play();
+        }
+    }
+
+
+    public void Play()
+    {
+        int loops = (loop) ? -1 : 0;
+        Vector2 positionTotal = (Vector2)transform.position + positionShift;
+        transform.DOMove(new Vector3(positionTotal.x, positionTotal.y, transform.position.z), time)
+                      .SetEase(ease)
+                      .SetLoops(loops, loopType);
+    }
+}
